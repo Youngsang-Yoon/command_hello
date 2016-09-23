79d4089e0d829883938611885d8962939562522c
ysyoon <yoonys05@naver.com>
2016-09-23 13:50:59 +0900

    hello.py modifyied on local repository

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
+//for command git ex
+//Local repository
+
 println ("Hello world")
 println ("my name is youngsang")
 println ("Hello your name")
 println ("hello git...")
+
+
+println ("hello py");
